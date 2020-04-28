Montellese/xbmc

[library] forward updates to video playcount/lastplayed/resumepoint to CMediaImportManager

xbmc/utils/SaveFileStateJob.cpp
@@ -33,6 +33,7 @@
 #include "guilib/GUIMessage.h"
 #include "guilib/GUIWindowManager.h"
 #include "GUIUserMessages.h"
+#include "media/import/MediaImportManager.h"
 #include "music/MusicDatabase.h"
 #include "cores/AudioEngine/Engines/ActiveAE/AudioDSPAddons/ActiveAEDSP.h"
 #include "xbmc/music/tags/MusicInfoTag.h"
@@ -55,7 +56,8 @@ bool CSaveFileStateJob::DoWork()
   {
 #ifdef HAS_UPNP
     // checks if UPnP server of this file is available and supports updating
-    if (URIUtils::IsUPnP(progressTrackingFile)
+    if (!m_item.IsImported() &&
+        URIUtils::IsUPnP(progressTrackingFile)
         && UPNP::CUPnP::SaveFileState(m_item, m_bookmark, m_updatePlayCount)) {
       return true;
     }
@@ -100,7 +102,10 @@ bool CSaveFileStateJob::DoWork()
             }
           }
           else
+          {
+            m_item.GetVideoInfoTag()->m_lastPlayed = CDateTime::GetCurrentDateTime();
             videodatabase.UpdateLastPlayed(m_item);
+          }
 
           if (!m_item.HasVideoInfoTag() || m_item.GetVideoInfoTag()->m_resumePoint.timeInSeconds != m_bookmark.timeInSeconds)
           {
@@ -226,6 +231,9 @@ bool CSaveFileStateJob::DoWork()
         audiodatabase.Close();
       }
     }
+
+    if (m_item.IsImported())
+      CMediaImportManager::GetInstance().UpdateImportedItem(m_item);
   }
   return true;
 }

fandangos/Kodi-HDR-Edition

xbmc/utils/SaveFileStateJob.cpp
@@ -21,6 +21,7 @@
 #include "guilib/GUIWindowManager.h"
 #include "interfaces/AnnouncementManager.h"
 #include "log.h"
+#include "media/import/MediaImportManager.h"
 #include "music/MusicDatabase.h"
 #include "music/tags/MusicInfoTag.h"
 #include "network/upnp/UPnP.h"
@@ -50,7 +51,8 @@ void CSaveFileState::DoWork(CFileItem& item,
   {
 #ifdef HAS_UPNP
     // checks if UPnP server of this file is available and supports updating
-    if (URIUtils::IsUPnP(progressTrackingFile)
+    if (!item.IsImported() &&
+        URIUtils::IsUPnP(progressTrackingFile)
         && UPNP::CUPnP::SaveFileState(item, bookmark, updatePlayCount))
     {
       return;
@@ -109,7 +111,11 @@ void CSaveFileState::DoWork(CFileItem& item,
             }
           }
           else
+          {
+            if (item.HasVideoInfoTag())
+              item.GetVideoInfoTag()->m_lastPlayed = CDateTime::GetCurrentDateTime();
             videodatabase.UpdateLastPlayed(item);
+          }
 
           if (!item.HasVideoInfoTag() ||
               item.GetVideoInfoTag()->GetResumePoint().timeInSeconds != bookmark.timeInSeconds)
@@ -207,5 +213,8 @@ void CSaveFileState::DoWork(CFileItem& item,
         musicdatabase.Close();
       }
     }
+
+    if (item.IsImported())
+      CServiceBroker::GetMediaImportManager().UpdateImportedItemOnSource(item);
   }
 }
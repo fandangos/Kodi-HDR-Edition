Montellese/xbmc

[network] upnp: add IMediaImporter implementation for UPnP items

xbmc/network/upnp/UPnP.cpp
@@ -28,6 +28,7 @@
 #include "threads/SystemClock.h"
 #include "UPnP.h"
 #include "UPnPInternal.h"
+#include "UPnPMediaImporter.h"
 #include "UPnPRenderer.h"
 #include "UPnPServer.h"
 #include "UPnPSettings.h"
@@ -44,11 +45,13 @@
 #include "GUIUserMessages.h"
 #include "FileItem.h"
 #include "guilib/GUIWindowManager.h"
+#include "utils/StringUtils.h"
 #include "utils/TimeUtils.h"
 #include "video/VideoInfoTag.h"
 #include "input/Key.h"
 #include "Util.h"
 #include "utils/SystemInfo.h"
+#include "media/import/MediaImportManager.h"
 
 using namespace UPNP;
 using namespace KODI::MESSAGING;
@@ -189,6 +192,13 @@ public:
         message.SetStringParam("upnp://");
         g_windowManager.SendThreadMessage(message);
 
+        if (CSettings::GetInstance().GetBool("services.upnpimport"))
+        {
+          std::string sourceID = getSourceID(device);
+          if (CMediaImportManager::GetInstance().AddSource(sourceID, sourceID, device->GetFriendlyName().GetChars(), device->GetIconUrl("image/png").GetChars(), { MediaTypeMovie, MediaTypeMusicVideo, MediaTypeTvShow, MediaTypeSeason, MediaTypeEpisode }))
+            CMediaImportManager::GetInstance().ActivateSource(sourceID, sourceID, device->GetFriendlyName().GetChars(), device->GetIconUrl("image/png").GetChars());
+        }
+
         return PLT_SyncMediaBrowser::OnMSAdded(device);
     }
     virtual void OnMSRemoved(PLT_DeviceDataReference& device)
@@ -198,6 +208,8 @@ public:
         CGUIMessage message(GUI_MSG_NOTIFY_ALL, 0, 0, GUI_MSG_UPDATE_PATH);
         message.SetStringParam("upnp://");
         g_windowManager.SendThreadMessage(message);
+
+        CMediaImportManager::GetInstance().DeactivateSource(getSourceID(device));
 
         PLT_SyncMediaBrowser::OnMSRemoved(device);
     }
@@ -351,6 +363,12 @@ public:
     failed:
         CLog::Log(LOGINFO, "UPNP: invoking UpdateObject failed");
         return false;
+    }
+
+private:
+    std::string getSourceID(const PLT_DeviceDataReference& device)
+    {
+      return StringUtils::Format("upnp://%s", device->GetUUID().GetChars());
     }
 };
 
@@ -645,6 +663,11 @@ CUPnP::StartClient()
 
     // start browser
     m_MediaBrowser = new CMediaBrowser(m_CtrlPointHolder->m_CtrlPoint);
+
+    // register the upnp media importer
+    if (m_mediaImporter == NULL)
+        m_mediaImporter = MediaImporterPtr(new CUPnPMediaImporter());
+    CMediaImportManager::GetInstance().RegisterImporter(m_mediaImporter);
 }
 
 /*----------------------------------------------------------------------
@@ -655,6 +678,10 @@ CUPnP::StopClient()
 {
     if (m_MediaBrowser == NULL)
         return;
+
+    // unregister the upnp media importer
+    if (m_mediaImporter != NULL)
+        CMediaImportManager::GetInstance().UnregisterImporter(m_mediaImporter);
 
     delete m_MediaBrowser;
     m_MediaBrowser = NULL;

fandangos/Kodi-HDR-Edition

xbmc/network/upnp/UPnP.cpp
@@ -16,6 +16,7 @@
 #include "GUIUserMessages.h"
 #include "ServiceBroker.h"
 #include "UPnPInternal.h"
+#include "UPnPMediaImporter.h"
 #include "UPnPRenderer.h"
 #include "UPnPServer.h"
 #include "UPnPSettings.h"
@@ -24,12 +25,14 @@
 #include "cores/playercorefactory/PlayerCoreFactory.h"
 #include "guilib/GUIComponent.h"
 #include "guilib/GUIWindowManager.h"
+#include "media/import/MediaImportManager.h"
 #include "messaging/ApplicationMessenger.h"
 #include "network/Network.h"
 #include "profiles/ProfileManager.h"
 #include "settings/Settings.h"
 #include "settings/SettingsComponent.h"
 #include "utils/StaticLoggerBase.h"
+#include "utils/StringUtils.h"
 #include "utils/SystemInfo.h"
 #include "utils/TimeUtils.h"
 #include "utils/URIUtils.h"
@@ -178,6 +181,14 @@ public:
         message.SetStringParam("upnp://");
         CServiceBroker::GetGUI()->GetWindowManager().SendThreadMessage(message);
 
+        if (CServiceBroker::GetSettingsComponent()->GetSettings()->GetBool("services.upnpimport"))
+        {
+          std::string sourceID = getSourceID(device);
+          CServiceBroker::GetMediaImportManager().AddAndActivateSource(CUPnPMediaImporterBase::IDENTIFICATION,
+            sourceID, sourceID, device->GetFriendlyName().GetChars(), device->GetIconUrl("image/png").GetChars(),
+            { MediaTypeMovie, MediaTypeMusicVideo, MediaTypeTvShow, MediaTypeSeason, MediaTypeEpisode });
+        }
+
         return PLT_SyncMediaBrowser::OnMSAdded(device);
     }
     void OnMSRemoved(PLT_DeviceDataReference& device) override
@@ -187,6 +198,8 @@ public:
         CGUIMessage message(GUI_MSG_NOTIFY_ALL, 0, 0, GUI_MSG_UPDATE_PATH);
         message.SetStringParam("upnp://");
         CServiceBroker::GetGUI()->GetWindowManager().SendThreadMessage(message);
+
+        CServiceBroker::GetMediaImportManager().DeactivateSource(getSourceID(device));
 
         PLT_SyncMediaBrowser::OnMSRemoved(device);
     }
@@ -351,6 +364,12 @@ public:
     failed:
       s_logger->info("invoking UpdateObject failed");
       return false;
+    }
+
+private:
+    std::string getSourceID(const PLT_DeviceDataReference& device)
+    {
+      return StringUtils::Format("upnp://%s", device->GetUUID().GetChars());
     }
 };
 
@@ -649,6 +668,11 @@ CUPnP::StartClient()
 
     // start browser
     m_MediaBrowser = new CMediaBrowser(m_CtrlPointHolder->m_CtrlPoint);
+
+    // register the upnp media importer
+    if (m_mediaImporterFactory == NULL)
+        m_mediaImporterFactory = std::make_shared<CUPnPMediaImporterFactory>();
+    CServiceBroker::GetMediaImportManager().RegisterImporter(m_mediaImporterFactory);
 }
 
 /*----------------------------------------------------------------------
@@ -659,6 +683,10 @@ CUPnP::StopClient()
 {
     if (m_MediaBrowser == NULL)
         return;
+
+    // unregister the upnp media importer
+    if (m_mediaImporterFactory != NULL)
+        CServiceBroker::GetMediaImportManager().UnregisterImporter(m_mediaImporterFactory->GetIdentification());
 
     delete m_MediaBrowser;
     m_MediaBrowser = NULL;
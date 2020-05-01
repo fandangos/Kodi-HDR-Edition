a171b23222ea61326be91e61f832ade3c2d5a3e0
mediabrasiltv <54152793+mediabrasiltv@users.noreply.github.com>
2020-04-30 22:20:55 -0300

    Update MediaImportEvent.cpp

--- xbmc/events/MediaImportEvent.cpp
+++ xbmc/events/MediaImportEvent.cpp
@@ -7,6 +7,7 @@
  */
 
 #include "MediaImportEvent.h"
+
 #include "ServiceBroker.h"
 #include "URL.h"
 #include "guilib/GUIComponent.h"
@@ -16,14 +17,22 @@
 #include "media/import/MediaImportManager.h"
 #include "utils/StringUtils.h"
 
-CMediaImportSourceEvent::CMediaImportSourceEvent(const CMediaImportSource& source, const CVariant& description, EventLevel level /* = EventLevel::Information */)
+CMediaImportSourceEvent::CMediaImportSourceEvent(const CMediaImportSource& source,
+                                                 const CVariant& description,
+                                                 EventLevel level /* = EventLevel::Information */)
   : CMediaImportSourceEvent(source, description, false, level)
-{ }
+{
+}
 
-CMediaImportSourceEvent::CMediaImportSourceEvent(const CMediaImportSource& source, const CVariant& description, bool removed, EventLevel level /* = EventLevel::Information */)
-  : CUniqueEvent(source.GetFriendlyName(), description, source.GetIconUrl(), CVariant{ removed }, level)
-  , m_source(source)
-{ }
+CMediaImportSourceEvent::CMediaImportSourceEvent(const CMediaImportSource& source,
+                                                 const CVariant& description,
+                                                 bool removed,
+                                                 EventLevel level /* = EventLevel::Information */)
+  : CUniqueEvent(
+        source.GetFriendlyName(), description, source.GetIconUrl(), CVariant{removed}, level),
+    m_source(source)
+{
+}
 
 std::string CMediaImportSourceEvent::GetExecutionLabel() const
 {
@@ -45,21 +54,34 @@ bool CMediaImportSourceEvent::Execute() const
     return false;
 
   std::vector<std::string> params;
-  params.push_back(StringUtils::Format("import://%s/", CURL::Encode(m_source.GetIdentifier()).c_str()));
+  params.push_back(
+      StringUtils::Format("import://%s/", CURL::Encode(m_source.GetIdentifier()).c_str()));
   params.push_back("return");
   CServiceBroker::GetGUI()->GetWindowManager().ActivateWindow(WINDOW_MEDIASOURCE_BROWSER, params);
   return true;
 }
 
-CMediaImportEvent::CMediaImportEvent(const CMediaImport& import, const CVariant& description, EventLevel level /* = EventLevel::Information */)
+CMediaImportEvent::CMediaImportEvent(const CMediaImport& import,
+                                     const CVariant& description,
+                                     EventLevel level /* = EventLevel::Information */)
   : CMediaImportEvent(import, description, false, level)
-{ }
+{
+}
 
-CMediaImportEvent::CMediaImportEvent(const CMediaImport& import, const CVariant& description, bool removed, EventLevel level /* = EventLevel::Information */)
-  : CUniqueEvent(StringUtils::Format(g_localizeStrings.Get(39565).c_str(), import.GetSource().GetFriendlyName().c_str(), CMediaTypes::ToLabel(import.GetMediaTypes()).c_str()),
-      description, import.GetSource().GetIconUrl(), CVariant{ removed }, level)
-  , m_import(import)
-{ }
+CMediaImportEvent::CMediaImportEvent(const CMediaImport& import,
+                                     const CVariant& description,
+                                     bool removed,
+                                     EventLevel level /* = EventLevel::Information */)
+  : CUniqueEvent(StringUtils::Format(g_localizeStrings.Get(39565).c_str(),
+                                     import.GetSource().GetFriendlyName().c_str(),
+                                     CMediaTypes::ToLabel(import.GetMediaTypes()).c_str()),
+                 description,
+                 import.GetSource().GetIconUrl(),
+                 CVariant{removed},
+                 level),
+    m_import(import)
+{
+}
 
 std::string CMediaImportEvent::GetExecutionLabel() const
 {
@@ -80,5 +102,6 @@ bool CMediaImportEvent::Execute() const
   if (!CanExecute())
     return false;
 
-  return CServiceBroker::GetMediaImportManager().Import(m_import.GetPath(), m_import.GetMediaTypes());
+  return CServiceBroker::GetMediaImportManager().Import(m_import.GetPath(),
+                                                        m_import.GetMediaTypes());
 }

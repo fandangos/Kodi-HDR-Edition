297611d200c281fd4c490a549003f527d664b540
mediabrasiltv <54152793+mediabrasiltv@users.noreply.github.com>
2020-04-30 22:10:41 -0300

    Update IMediaImportHandler.h

--- xbmc/media/import/IMediaImportHandler.h
+++ xbmc/media/import/IMediaImportHandler.h
@@ -45,7 +45,7 @@ public:
    * \brief Returns a list of media types which can be grouped together
    * with the media type of this implementation.
    */
-  virtual GroupedMediaTypes GetGroupedMediaTypes() const { return { GetMediaType() }; }
+  virtual GroupedMediaTypes GetGroupedMediaTypes() const { return {GetMediaType()}; }
 
   /*!
    * \brief Get the translated label representing the given item
@@ -61,7 +61,8 @@ public:
    * \param import Place from where the given items were imported
    * \param items List of previously imported items from the given media import
    */
-  virtual bool GetLocalItems(const CMediaImport &import, std::vector<CFileItemPtr>& items) const = 0;
+  virtual bool GetLocalItems(const CMediaImport& import,
+                             std::vector<CFileItemPtr>& items) const = 0;
 
   /*!
    * \brief Starts the task determining the changeset of the imported items against previously imported items.
@@ -69,7 +70,7 @@ public:
    * \param import Place from where the given items were imported
    * \return True if the preparations were successful, false otherwise
    */
-  virtual bool StartChangeset(const CMediaImport &import) = 0;
+  virtual bool StartChangeset(const CMediaImport& import) = 0;
 
   /*!
    * \brief Finishes the task determining the changeset of the imported items against previously imported items.
@@ -77,7 +78,7 @@ public:
    * \param import Place from where the given items were imported
    * \return True if the finalizations were successful, false otherwise
    */
-  virtual bool FinishChangeset(const CMediaImport &import) = 0;
+  virtual bool FinishChangeset(const CMediaImport& import) = 0;
 
   /*!
    * \brief Tries to find a previously imported item matching the given item
@@ -87,7 +88,9 @@ public:
    * \param localItems Previously imported items
    * \return The matching previously imported item or nullptr
    */
-  virtual CFileItemPtr FindMatchingLocalItem(const CMediaImport &import, const CFileItem* item, const std::vector<CFileItemPtr>& localItems) const = 0;
+  virtual CFileItemPtr FindMatchingLocalItem(const CMediaImport& import,
+                                             const CFileItem* item,
+                                             const std::vector<CFileItemPtr>& localItems) const = 0;
 
   /*!
    * \brief Determining the changeset of the imported item against the previously imported item.
@@ -97,8 +100,10 @@ public:
    * \param localItem Previously imported item
    * \return Type of the changeset of the two imported items
    */
-  virtual MediaImportChangesetType DetermineChangeset(const CMediaImport &import, const CFileItem* item, const CFileItemPtr& localItem) = 0;
-  
+  virtual MediaImportChangesetType DetermineChangeset(const CMediaImport& import,
+                                                      const CFileItem* item,
+                                                      const CFileItemPtr& localItem) = 0;
+
   /*!
    * \brief Prepares the given imported item for updating based on the previously imported item.
    *
@@ -106,7 +111,9 @@ public:
    * \param item Imported item to be prepared
    * \param localItem Previously imported item
    */
-  virtual void PrepareImportedItem(const CMediaImport& import, CFileItem* item, const CFileItemPtr& localItem) const = 0;
+  virtual void PrepareImportedItem(const CMediaImport& import,
+                                   CFileItem* item,
+                                   const CFileItemPtr& localItem) const = 0;
 
   /*!
    * \brief Starts the synchronisation process
@@ -114,7 +121,7 @@ public:
    * \param import Place from where the given items were imported
    * \return True if the preparations were successful, false otherwise
    */
-  virtual bool StartSynchronisation(const CMediaImport &import) = 0;
+  virtual bool StartSynchronisation(const CMediaImport& import) = 0;
 
   /*!
    * \brief Finishes the synchronisation process
@@ -122,7 +129,7 @@ public:
    * \param import Place from where the given items were imported
    * \return True if the finalizations were successful, false otherwise
    */
-  virtual bool FinishSynchronisation(const CMediaImport &import) = 0;
+  virtual bool FinishSynchronisation(const CMediaImport& import) = 0;
 
   /*!
    * \brief Adds the given item from the given import to the library
@@ -131,7 +138,7 @@ public:
    * \param item Imported item to be added to the library
    * \return True if the item was successfully added, false otherwise
    */
-  virtual bool AddImportedItem(const CMediaImport &import, CFileItem* item) = 0;
+  virtual bool AddImportedItem(const CMediaImport& import, CFileItem* item) = 0;
 
   /*!
    * \brief Updates the given item from the given import in the library
@@ -140,7 +147,7 @@ public:
    * \param item Imported item to be updated in the library
    * \return True if the item was successfully updated, false otherwise
    */
-  virtual bool UpdateImportedItem(const CMediaImport &import, CFileItem* item) = 0;
+  virtual bool UpdateImportedItem(const CMediaImport& import, CFileItem* item) = 0;
 
   /*!
    * \brief Removes the given item from the given import from the library
@@ -149,7 +156,7 @@ public:
    * \param item Imported item to be removed from the library
    * \return True if the item was successfully removed, false otherwise
    */
-  virtual bool RemoveImportedItem(const CMediaImport &import, const CFileItem* item) = 0;
+  virtual bool RemoveImportedItem(const CMediaImport& import, const CFileItem* item) = 0;
 
   /*!
    * \brief Cleans up the imported items in the library
@@ -157,7 +164,7 @@ public:
    * \param import Place from where the given items were imported
    * \return True if the library was successfully cleaned up, false otherwise
    */
-  virtual bool CleanupImportedItems(const CMediaImport &import) = 0;
+  virtual bool CleanupImportedItems(const CMediaImport& import) = 0;
 
   /*!
    * \brief Removes all imported items from the library
@@ -165,7 +172,7 @@ public:
    * \param import Place from where the given items were imported
    * \return True if the imported items were successfully removed from the library, false otherwise
    */
-  virtual bool RemoveImportedItems(const CMediaImport &import) = 0;
+  virtual bool RemoveImportedItems(const CMediaImport& import) = 0;
 
   /*!
    * \brief Enable/disable imported items
@@ -173,12 +180,13 @@ public:
    * \param import Place from where the given items were imported
    * \param enable Whether to enable or disable imported items
    */
-  virtual void SetImportedItemsEnabled(const CMediaImport &import, bool enable) = 0;
+  virtual void SetImportedItemsEnabled(const CMediaImport& import, bool enable) = 0;
 
 protected:
   IMediaImportHandler(const IMediaImportHandlerManager* importHandlerManager)
     : m_importHandlerManager(importHandlerManager)
-  { }
+  {
+  }
 
   const IMediaImportHandlerManager* m_importHandlerManager;
 };

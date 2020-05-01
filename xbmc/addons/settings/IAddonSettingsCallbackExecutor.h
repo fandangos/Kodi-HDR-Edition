acc6e75d39be51b42a434a712d895a9d96e72ac9
mediabrasiltv <54152793+mediabrasiltv@users.noreply.github.com>
2020-04-30 22:19:50 -0300

    Update IAddonSettingsCallbackExecutor.h

--- xbmc/addons/settings/IAddonSettingsCallbackExecutor.h
+++ xbmc/addons/settings/IAddonSettingsCallbackExecutor.h
@@ -24,22 +24,43 @@ public:
 
   virtual bool LoadSettings(std::shared_ptr<CSettingsBase> settings, void* data) { return false; }
   virtual bool SaveSettings(std::shared_ptr<CSettingsBase> settings, void* data) { return false; }
-  virtual void SetSettingsLoaded(void* data) { }
+  virtual void SetSettingsLoaded(void* data) {}
 
-  virtual void OnSettingAction(std::shared_ptr<const CSetting> setting, const std::string& callback, void* data) { }
+  virtual void OnSettingAction(std::shared_ptr<const CSetting> setting,
+                               const std::string& callback,
+                               void* data)
+  {
+  }
 
   virtual void OnIntegerSettingOptionsFiller(std::shared_ptr<const CSetting> setting,
-    const std::string& callback, IntegerSettingOptions& list, int& current, void* data) { }
+                                             const std::string& callback,
+                                             IntegerSettingOptions& list,
+                                             int& current,
+                                             void* data)
+  {
+  }
   virtual bool SetIntegerSettingOptions(const std::string& settingId,
-    const IntegerSettingOptions& list, void* data) { return false; }
+                                        const IntegerSettingOptions& list,
+                                        void* data)
+  {
+    return false;
+  }
 
   virtual void OnStringSettingOptionsFiller(std::shared_ptr<const CSetting> setting,
-    const std::string& callback, StringSettingOptions& list, std::string& current, void* data) { }
+                                            const std::string& callback,
+                                            StringSettingOptions& list,
+                                            std::string& current,
+                                            void* data)
+  {
+  }
   virtual bool SetStringSettingOptions(const std::string& settingId,
-    const StringSettingOptions& list, void* data) { return false; }
+                                       const StringSettingOptions& list,
+                                       void* data)
+  {
+    return false;
+  }
 
 protected:
   IAddonSettingsCallbackExecutor() = default;
 };
-
-}
+} // namespace ADDON

d06da5c79f96576c8e51d588facb4d38f5937163
mediabrasiltv <54152793+mediabrasiltv@users.noreply.github.com>
2020-04-30 22:29:55 -0300

    Update ScriptRunner.h

--- xbmc/interfaces/generic/ScriptRunner.h
+++ xbmc/interfaces/generic/ScriptRunner.h
@@ -24,16 +24,26 @@ protected:
 
   ADDON::AddonPtr GetAddon() const { return m_addon; }
 
-  inline bool StartScript(ADDON::AddonPtr addon, const std::string& path) { return RunScriptInternal(addon, path, 0, false); }
-  inline bool RunScript(ADDON::AddonPtr addon, const std::string& path, int handle, bool resume) { return RunScriptInternal(addon, path, handle, resume, true); }
+  inline bool StartScript(ADDON::AddonPtr addon, const std::string& path)
+  {
+    return RunScriptInternal(addon, path, 0, false);
+  }
+  inline bool RunScript(ADDON::AddonPtr addon, const std::string& path, int handle, bool resume)
+  {
+    return RunScriptInternal(addon, path, handle, resume, true);
+  }
 
   void SetDone();
 
-  static inline int ExecuteScript(ADDON::AddonPtr addon, const std::string& path, bool resume) { return ExecuteScript(addon, path, -1, resume); }
+  static inline int ExecuteScript(ADDON::AddonPtr addon, const std::string& path, bool resume)
+  {
+    return ExecuteScript(addon, path, -1, resume);
+  }
   static int ExecuteScript(ADDON::AddonPtr addon, const std::string& path, int handle, bool resume);
 
 private:
-  bool RunScriptInternal(ADDON::AddonPtr addon, const std::string& path, int handle, bool resume, bool wait = true);
+  bool RunScriptInternal(
+      ADDON::AddonPtr addon, const std::string& path, int handle, bool resume, bool wait = true);
   bool WaitOnScriptResult(int scriptId, const std::string& path, const std::string& name);
 
   ADDON::AddonPtr m_addon;

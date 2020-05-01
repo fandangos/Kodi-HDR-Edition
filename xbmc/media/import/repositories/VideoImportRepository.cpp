950e5d5f122583a0fe6f3f39c4d8334f8750cafc
mediabrasiltv <54152793+mediabrasiltv@users.noreply.github.com>
2020-04-30 23:05:55 -0300

    Update VideoImportRepository.cpp

--- xbmc/media/import/repositories/VideoImportRepository.cpp
+++ xbmc/media/import/repositories/VideoImportRepository.cpp
@@ -28,17 +28,17 @@ std::vector<CMediaImportSource> CVideoImportRepository::getSources() const
   return m_db.GetSources();
 }
 
-int CVideoImportRepository::addSource(const CMediaImportSource &source)
+int CVideoImportRepository::addSource(const CMediaImportSource& source)
 {
   return m_db.AddSource(source);
 }
 
-bool CVideoImportRepository::updateSource(const CMediaImportSource &source)
+bool CVideoImportRepository::updateSource(const CMediaImportSource& source)
 {
   return m_db.SetDetailsForSource(source);
 }
 
-void CVideoImportRepository::removeSource(const CMediaImportSource &source)
+void CVideoImportRepository::removeSource(const CMediaImportSource& source)
 {
   m_db.RemoveSource(source.GetIdentifier());
 }
@@ -48,22 +48,22 @@ std::vector<CMediaImport> CVideoImportRepository::getImports() const
   return m_db.GetImports();
 }
 
-int CVideoImportRepository::addImport(const CMediaImport &import)
+int CVideoImportRepository::addImport(const CMediaImport& import)
 {
   return m_db.AddImport(import);
 }
 
-bool CVideoImportRepository::updateImport(const CMediaImport &import)
+bool CVideoImportRepository::updateImport(const CMediaImport& import)
 {
   return m_db.SetDetailsForImport(import);
 }
 
-void CVideoImportRepository::removeImport(const CMediaImport &import)
+void CVideoImportRepository::removeImport(const CMediaImport& import)
 {
   m_db.RemoveImport(import);
 }
 
-void CVideoImportRepository::updateLastSync(const CMediaImport &import, const CDateTime &lastSync)
+void CVideoImportRepository::updateLastSync(const CMediaImport& import, const CDateTime& lastSync)
 {
   m_db.UpdateImportLastSynced(import, lastSync);
 }

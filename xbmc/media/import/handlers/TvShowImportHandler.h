fb3f8a206f2d91e814c6c822de3f4aa5ea1ece43
mediabrasiltv <54152793+mediabrasiltv@users.noreply.github.com>
2020-04-30 22:52:01 -0300

    Update TvShowImportHandler.h

--- xbmc/media/import/handlers/TvShowImportHandler.h
+++ xbmc/media/import/handlers/TvShowImportHandler.h
@@ -15,27 +15,40 @@ class CTvShowImportHandler : public CVideoImportHandler
 public:
   CTvShowImportHandler(const IMediaImportHandlerManager* importHandlerManager)
     : CVideoImportHandler(importHandlerManager)
-  { }
+  {
+  }
   virtual ~CTvShowImportHandler() = default;
 
-  CTvShowImportHandler* Create() const override { return new CTvShowImportHandler(m_importHandlerManager); }
+  CTvShowImportHandler* Create() const override
+  {
+    return new CTvShowImportHandler(m_importHandlerManager);
+  }
 
   MediaType GetMediaType() const override { return MediaTypeTvShow; }
-  MediaTypes GetRequiredMediaTypes() const override { return { MediaTypeEpisode }; }
-  GroupedMediaTypes GetGroupedMediaTypes() const override { return { MediaTypeTvShow, MediaTypeSeason, MediaTypeEpisode }; }
+  MediaTypes GetRequiredMediaTypes() const override { return {MediaTypeEpisode}; }
+  GroupedMediaTypes GetGroupedMediaTypes() const override
+  {
+    return {MediaTypeTvShow, MediaTypeSeason, MediaTypeEpisode};
+  }
 
-  CFileItemPtr FindMatchingLocalItem(const CMediaImport &import, const CFileItem* item, const std::vector<CFileItemPtr>& localItems) const override;
+  CFileItemPtr FindMatchingLocalItem(const CMediaImport& import,
+                                     const CFileItem* item,
+                                     const std::vector<CFileItemPtr>& localItems) const override;
 
-  bool AddImportedItem(const CMediaImport &import, CFileItem* item) override;
-  bool UpdateImportedItem(const CMediaImport &import, CFileItem* item) override;
-  bool RemoveImportedItem(const CMediaImport &import, const CFileItem* item) override;
-  bool CleanupImportedItems(const CMediaImport &import) override;
+  bool AddImportedItem(const CMediaImport& import, CFileItem* item) override;
+  bool UpdateImportedItem(const CMediaImport& import, CFileItem* item) override;
+  bool RemoveImportedItem(const CMediaImport& import, const CFileItem* item) override;
+  bool CleanupImportedItems(const CMediaImport& import) override;
 
 protected:
-  bool GetLocalItems(CVideoDatabase &videodb, const CMediaImport &import, std::vector<CFileItemPtr>& items) const override;
+  bool GetLocalItems(CVideoDatabase& videodb,
+                     const CMediaImport& import,
+                     std::vector<CFileItemPtr>& items) const override;
 
   std::set<Field> IgnoreDifferences() const override;
 
-  bool RemoveImportedItems(CVideoDatabase &videodb, const CMediaImport &import) const override;
-  void RemoveImportedItem(CVideoDatabase &videodb, const CMediaImport &import, const CFileItem* item) const;
+  bool RemoveImportedItems(CVideoDatabase& videodb, const CMediaImport& import) const override;
+  void RemoveImportedItem(CVideoDatabase& videodb,
+                          const CMediaImport& import,
+                          const CFileItem* item) const;
 };

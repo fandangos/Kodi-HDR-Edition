b48542198592f3e965d086c0c988948159fd998c
mediabrasiltv <54152793+mediabrasiltv@users.noreply.github.com>
2020-04-30 22:46:36 -0300

    Update MovieSetImportHandler.cpp

--- xbmc/media/import/handlers/MovieSetImportHandler.cpp
+++ xbmc/media/import/handlers/MovieSetImportHandler.cpp
@@ -1,118 +1,44 @@
-/*MovieSetImportHandler
- *  Copyright (C) 2020 Team Kodi
+/*
+ *  Copyright (C) 2013-2019 Team Kodi
  *  This file is part of Kodi - https://kodi.tv
  *
  *  SPDX-License-Identifier: GPL-2.0-or-later
  *  See LICENSES/README.md for more information.
  */
 
-#include "MovieSetImportHandler.h"
+#pragma once
 
-#include "FileItem.h"
-#include "media/import/MediaImport.h"
-#include "video/VideoDatabase.h"
+#include "media/import/handlers/VideoImportHandler.h"
 
-CFileItemPtr CMovieSetImportHandler::FindMatchingLocalItem(
-  const CMediaImport& import, const CFileItem* item, const std::vector<CFileItemPtr>& localItems) const
+class CMovieImportHandler : public CVideoImportHandler
 {
-  if (item == nullptr || !item->HasVideoInfoTag())
-    return nullptr;
+public:
+  CMovieImportHandler(const IMediaImportHandlerManager* importHandlerManager)
+    : CVideoImportHandler(importHandlerManager)
+  {
+  }
+  virtual ~CMovieImportHandler() = default;
 
-  const auto& localItem = std::find_if(localItems.cbegin(), localItems.cend(),
-    [&item](const CFileItemPtr& localItem)
-    {
-      return item->GetVideoInfoTag()->m_strTitle == localItem->GetVideoInfoTag()->m_strTitle;
-    });
+  CMovieImportHandler* Create() const override
+  {
+    return new CMovieImportHandler(m_importHandlerManager);
+  }
 
-  if (localItem != localItems.cend())
-    return *localItem;
+  MediaType GetMediaType() const override { return MediaTypeMovie; }
+  GroupedMediaTypes GetGroupedMediaTypes() const override
+  {
+    return {MediaTypeMovie, MediaTypeVideoCollection};
+  }
 
-  return nullptr;
-}
+  bool AddImportedItem(const CMediaImport& import, CFileItem* item) override;
+  bool UpdateImportedItem(const CMediaImport& import, CFileItem* item) override;
+  bool RemoveImportedItem(const CMediaImport& import, const CFileItem* item) override;
+  bool CleanupImportedItems(const CMediaImport& import) override { return true; }
 
-bool CMovieSetImportHandler::AddImportedItem(const CMediaImport &import, CFileItem* item)
-{
-  if (item == nullptr)
-    return false;
+protected:
+  bool GetLocalItems(CVideoDatabase& videodb,
+                     const CMediaImport& import,
+                     std::vector<CFileItemPtr>& items) const override;
 
-  PrepareItem(import, item);
-
-  item->GetVideoInfoTag()->m_iDbId = m_db.SetDetailsForMovieSet(*(item->GetVideoInfoTag()), item->GetArt());
-  if (item->GetVideoInfoTag()->m_iDbId <= 0)
-    return false;
-
-  return SetImportForItem(item, import);
-}
-
-bool CMovieSetImportHandler::UpdateImportedItem(const CMediaImport &import, CFileItem* item)
-{
-  if (item == nullptr || !item->HasVideoInfoTag() || item->GetVideoInfoTag()->m_iDbId <= 0)
-    return false;
-
-  if (m_db.SetDetailsForMovieSet(*(item->GetVideoInfoTag()), item->GetArt(), item->GetVideoInfoTag()->m_iDbId) <= 0)
-    return false;
-
-  return true;
-}
-
-bool CMovieSetImportHandler::RemoveImportedItem(const CMediaImport &import, const CFileItem* item)
-{
-  if (item == nullptr || !item->HasVideoInfoTag())
-    return false;
-
-  m_db.DeleteSet(item->GetVideoInfoTag()->m_iDbId);
-
-  return true;
-}
-
-bool CMovieSetImportHandler::GetLocalItems(CVideoDatabase &videodb, const CMediaImport &import, std::vector<CFileItemPtr>& items) const
-{
-  CFileItemList movieSets;
-  if (!videodb.GetSetsByWhere("videodb://movies/sets/?imported&import=" + CURL::Encode(import.GetPath()), CDatabase::Filter(), movieSets, false))
-    return false;
-
-  items.insert(items.begin(), movieSets.cbegin(), movieSets.cend());
-
-  return true;
-}
-
-std::set<Field> CMovieSetImportHandler::IgnoreDifferences() const
-{
-  return {
-    FieldActor,
-    FieldAirDate,
-    FieldAlbum,
-    FieldArtist,
-    FieldCountry,
-    FieldDirector,
-    FieldEpisodeNumber,
-    FieldEpisodeNumberSpecialSort,
-    FieldFilename,
-    FieldGenre,
-    FieldInProgress,
-    FieldLastPlayed,
-    FieldMPAA,
-    FieldOriginalTitle,
-    FieldPath,
-    FieldPlaycount,
-    FieldPlotOutline,
-    FieldProductionCode,
-    FieldRating,
-    FieldSeason,
-    FieldSeasonSpecialSort,
-    FieldSet,
-    FieldSortTitle,
-    FieldStudio,
-    FieldTag,
-    FieldTagline,
-    FieldTime,
-    FieldTop250,
-    FieldTrackNumber,
-    FieldTrailer,
-    FieldTvShowStatus,
-    FieldTvShowTitle,
-    FieldUniqueId,
-    FieldUserRating,
-    FieldWriter
-  };
-}
+  std::set<Field> IgnoreDifferences() const override;
+};
